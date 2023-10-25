4371c5369b623442d5ea652e2c0a3f1555b88ad6
muthuraman2002 <128206014+muthuraman2002@users.noreply.github.com>
2023-10-25 13:58:27 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,7 @@
 duplicate_student={}
 student={"Bharathi":"25","muthu":"100","siva":"90","bhabi":"50"}
 for key,value in student.items():
-    if  int(value)>30:
+    if  int(value)>35:
         duplicate_student[key] = value
 print(duplicate_student)
     
